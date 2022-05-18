fd64c132025c6518bf16b5a09c237300c46ba56b
carlosjtandem <soycarlosjavier14@gmail.com>
2022-05-18 18:03:31 -0500

    added project

---
+++ Migrations/20220518185551_ModelRevisions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    public partial class ModelRevisions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVegan",
+                table: "Sauces",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVegan",
+                table: "Sauces");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,13 @@
+namespace ContosoPizza.Models;
+using System.ComponentModel.DataAnnotations;
+public class Pizza
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string? Name { get; set; }
+
+    public Sauce? Sauce { get; set; }
+
+    public ICollection<Topping>? Toppings { get; set; }
+}
